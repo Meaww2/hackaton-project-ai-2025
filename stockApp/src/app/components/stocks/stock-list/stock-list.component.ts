769f8eb9948d50aa0a971461658c3e7f7de0aeee
Jinnawat02 <tottnt765@gmail.com>
2025-09-26 17:31:30 +0700

    fix design and add logout btn

--- stockApp/src/app/components/stocks/stock-list/stock-list.component.ts
+++ stockApp/src/app/components/stocks/stock-list/stock-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from "../../../services/auth.service";
 
 @Component({
   selector: 'app-stock-list',
@@ -8,7 +9,14 @@ import { Component } from '@angular/core';
 export class StockListComponent {
   currentTab: string = 'dashboard'; // ค่า default
 
+  constructor(private authService: AuthService) {}
+
   setTab(tab: string) {
     this.currentTab = tab;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    window.location.reload();
   }
 }
