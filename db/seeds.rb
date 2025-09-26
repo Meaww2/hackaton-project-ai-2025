d9909302719c386f4812881e14c680ad54df4318
Jinnawat02 <tottnt765@gmail.com>
2025-09-26 14:06:32 +0700

    init devise, rolify and cancancan

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Role.find_or_create_by(name: :manager)
+Role.find_or_create_by(name: :warehouse_staff)
+Role.find_or_create_by(name: :employee)
+
+u1 = User.create!(email: 'manager@example.com', password: '123456')
+u1.add_role :manager
+
+u2 = User.create!(email: 'staff@example.com', password: '123456')
+u2.add_role :warehouse_staff
+
+u3 = User.create!(email: 'employee@example.com', password: '123456')
+u3.add_role :employee
