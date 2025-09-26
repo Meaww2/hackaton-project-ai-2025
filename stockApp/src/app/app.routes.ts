383b2692fd0d7cc758e0bbc2fb541d9e4b0e3829
Khom <siwakorn@bananacoding.com>
2025-09-26 15:03:28 +0700

    Add ManagementComponent with routing and basic tests

--- stockApp/src/app/app.routes.ts
+++ stockApp/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ManagementComponent } from './components/management/management.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'management',
+        component: ManagementComponent
+    }
+];
