a3f9959ea9c889ce822820ac40a643fd2ad153ca
Rachata Thananchai <childenchaos@gmail.com>
2025-09-26 15:01:21 +0700

    create home page

--- stockApp/src/app/app.module.ts
+++ stockApp/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DropdownModule } from 'primeng/dropdown';
+import { StockListComponent } from './components/stocks/stock-list/stock-list.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    StockListComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     DropdownModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- stockApp/src/app/app.routes.ts
+++ stockApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StockListComponent } from './components/stocks/stock-list/stock-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'index',
+        component: StockListComponent,
+    }
+];
